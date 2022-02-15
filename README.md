1758f705e623ae13771035884ac2a73a2a016b8f
Colin Ayres <CAyres7@gmail.com>
2022-02-14 23:45:14 -0500

    Added basic UI via DOM manipulation

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# RockPaperScissors
+A basic Rock, Paper, Scissors game. Practicing with JavaScript; adding UI elements via DOM manipulation.
--- script.js
+++ script.js
@@ -1,44 +1,100 @@
-game();
+let pscoreCount = 0; //Player Score Count 
+let cscoreCount = 0; //Computer Score Count
+let roundCount = 0; //Holds count of Round
+const bcontainer = document.querySelector('#bcontainer');
+const container = document.querySelector('#container');
+const content = document.createElement('div');
+const scoreContent = document.createElement('div');
+const roundContent = document.createElement('div');
+const winnerContent = document.createElement('div');
+content.classList.add('content')
+scoreContent.classList.add('content')
+roundContent.classList.add('content')
+winnerContent.classList.add('content')
 
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    let i = 1;
-        for (i = 1; i < 6; i++){
-            promptText = prompt("Rock, Paper, or Scissors?");
-            userInput = promptText.toLowerCase();
-            computerInput = computerPlay();
-            results = playRound(userInput, computerInput);
-            switch (true)
-            {
-                case (results.substring(0,5)==="You w"):
-                    playerScore++;
-                    console.log("Computer chooses: " + computerInput + ". You won round " + i + "!");
-                    break;
-                case (results.substring(0,5)==="You l"):
-                    computerScore++;
-                    console.log("Computer chooses: " + computerInput + ". You lost round " + i + ".");
-                    break;
-                case (results.substring(0,5)==="Draw!"):
-                    console.log("Computer chooses: " + computerInput + ". You drew on round " + i + ".");
-                    break;
-                case (results.substring(0,5)==="Inval"):
-                    console.log("Invalid input this round, try again.");
-                    i--;    
-                    break;
-            }
-        console.log("Player: " + playerScore + "  Computer: " + computerScore)
-        }
-    switch (i==6)
-    {
-        case (playerScore > computerScore):
-            console.log("You won the game!")
-            break;
-        case (computerScore > playerScore):
-            console.log("You lost the game!")
-            break;
-        case (computerScore == playerScore):
-            console.log("The game was a draw!")
+winnerContent.textContent = "Choose rock, paper, or scissors to beat the computer. 5 rounds!"
+scoreContent.textContent = "Player Score: " + pscoreCount + ";" + " Computer Score: " + cscoreCount;
+roundContent.textContent = "Round: " + roundCount;
+bcontainer.appendChild(winnerContent);
+bcontainer.appendChild(scoreContent);
+bcontainer.appendChild(roundContent); 
+content.classList.add('content');
+//Add rock button
+const rockButton = document.createElement("button");
+rockButton.classList.add('rockButton');
+rockButton.innerHTML = "Rock!";
+rockButton.type = "submit";
+rockButton.name = "formBtn";
+rockButton.onclick = function () {
+    computerInput = computerPlay();
+    userInput = "rock";
+    whoWon(playRound(userInput, computerInput));
+    winnerContent.textContent = playRound(userInput, computerInput);
+    scoreContent.textContent = "Player Score: " + pscoreCount + ";" + " Computer Score: " + cscoreCount;
+    roundContent.textContent = "Round: " + roundCount;
+    if (roundCount>4){
+        content.textContent = "";
+        endGame();
+    }
+  };
+container.appendChild(rockButton)
+//Add paper button; on click, execute computerPlay and playRound using user input. Display results and track score.
+const paperButton = document.createElement("button");
+paperButton.classList.add('paperButton');
+paperButton.innerHTML = "Paper!";
+paperButton.type = "submit";
+paperButton.name = "formBtn";
+paperButton.onclick = function () {
+    computerInput = computerPlay();
+    userInput = "paper";
+    whoWon(playRound(userInput, computerInput));
+    winnerContent.textContent = playRound(userInput, computerInput);
+    scoreContent.textContent = "Player Score: " + pscoreCount + ";" + " Computer Score: " + cscoreCount;
+    roundContent.textContent = "Round: " + roundCount;
+    if (roundCount>4){
+        content.textContent = "";
+        endGame();
+    }
+  };
+container.appendChild(paperButton)
+//Add scissors button
+const scissorsButton = document.createElement("button");
+scissorsButton.classList.add('scissorsButton');
+scissorsButton.innerHTML = "Scissors!";
+scissorsButton.type = "submit";
+scissorsButton.name = "formBtn";
+scissorsButton.onclick = function () {
+    computerInput = computerPlay();
+    userInput = "scissors";
+    whoWon(playRound(userInput, computerInput));
+    winnerContent.textContent = playRound(userInput, computerInput);
+    scoreContent.textContent = "Player Score: " + pscoreCount + ";" + " Computer Score: " + cscoreCount;
+    roundContent.textContent = "Round: " + roundCount;
+    if (roundCount>4){
+        content.textContent = "";
+        endGame();
+    }
+  };
+container.appendChild(scissorsButton)
+
+function endGame(){
+    if (pscoreCount > cscoreCount){
+        winnerContent.textContent = "You won after 5 rounds! Choose rock, paper, or scissors to start a new game."
+        pscoreCount = 0;
+        cscoreCount = 0;
+        roundCount = 0;
+    }
+    else if (pscoreCount < cscoreCount){
+        winnerContent.textContent = "You lost after 5 rounds! Choose rock, paper, or scissors to start a new game."
+        pscoreCount = 0;
+        cscoreCount = 0;
+        roundCount = 0;
+    } 
+    else if (pscoreCount == cscoreCount){
+        winnerContent.textContent = "You drew after 5 rounds! Choose rock, paper, or scissors to start a new game."
+        pscoreCount = 0;
+        cscoreCount = 0;
+        roundCount = 0;     
     }
 }
 
@@ -46,19 +102,19 @@ function playRound(playerSelection, computerSelection){
     switch (true) 
     {
         case (playerSelection === "rock" && computerSelection === "paper"):
-            return "You lose! Paper beats rock.";
+            return "You lose this round! Paper beats rock.";
         case (playerSelection === "paper" && computerSelection === "rock"):
-            return "You win! Paper beats rock.";
+            return "You win this round! Paper beats rock.";
         case (playerSelection === "rock" && computerSelection === "scissors"):
-            return "You win! Rock beats scissors.";
+            return "You win this round! Rock beats scissors.";
         case (playerSelection === "scissors" && computerSelection === "rock"):
-            return "You lose! Rock beats scissors.";
+            return "You lose this round! Rock beats scissors.";
         case (playerSelection === "scissors" && computerSelection === "paper"):
-            return "You win! Scissors beats paper.";
+            return "You win this round! Scissors beats paper.";
         case (playerSelection === "paper" && computerSelection === "scissors"):
-            return "You lose! Scissors beats paper.";
+            return "You lose this round! Scissors beats paper.";
         case (playerSelection === computerSelection):
-            return "Draw!";
+            return "Draw this round!";
         case (true):
             return "Invalid input.";
     }
@@ -74,7 +130,22 @@ function computerPlay(){
             return "rock";
         case 2:
             return "scissors";
-        case (true):
-            break;
+    }
+}
+
+function whoWon(input){
+    switch (true)
+    {
+        case (input.substring(0,5)==="You w"):
+            pscoreCount++;
+            roundCount++;
+            return 1;
+        case (input.substring(0,5)==="You l"):
+            cscoreCount++;
+            roundCount++;
+            return -1;
+        case (input.substring(0,5)==="Draw "):
+            roundCount++;
+            return 0;    
     }
 }
